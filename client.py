5abf797a66469e7f27783f9c2a366479401d597e
Joseph Bongrand <josephbongrand@Josephs-MacBook-Pro.local>
2019-12-19 14:20:21 +0100

    Paramétrage des valeurs et envoie vers le serveur

--- client.py
+++ client.py
@@ -7,19 +7,13 @@ connexion_avec_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connexion_avec_serveur.connect((hote, port))
 print("Connexion établie avec le serveur sur le port {}".format(port))
 
+valeur1 = input("Valeur 1 = ").encode()
+valeur2 = input("Valeur 2 = ").encode()
+valeur3 = input("Valeur 3 = ").encode()
 
-connexion_avec_serveur.send(input(int("valeur1 : ")))
-connexion_avec_serveur.send(input("+", "-", "*", "/"))
-connexion_avec_serveur.send(input(int("valeur3 : ")))
-# msg_a_envoyer = b""
-# while msg_a_envoyer != b"fin":
-#    msg_a_envoyer = input("> ")
-#    # Peut planter si vous tapez des caractères spéciaux
-#    msg_a_envoyer = msg_a_envoyer.encode()
-#    # On envoie le message
-#    connexion_avec_serveur.send(msg_a_envoyer)
-#    msg_recu = connexion_avec_serveur.recv(1024)
-#    print(msg_recu.decode())  # Là encore, peut planter s'il y a des accents
+connexion_avec_serveur.send(valeur1)
+connexion_avec_serveur.send(valeur2)
+connexion_avec_serveur.send(valeur3)
 
 print("Fermeture de la connexion")
 connexion_avec_serveur.close()
